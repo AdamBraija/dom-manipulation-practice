660803ce6b4530adea3ad1d73bfe6a3d1ca14118
Adam Braija <adam.braija1@gmail.com>
2026-03-11 19:51:46 +0000

    Fix image paths and remove duplicate nested project folder

--- dom-manipulation-practice/image-switch/script.js
+++
@@ -1,9 +0,0 @@
-const image = document.getElementById("mainImage");
-const pictures = ["../images/pic2.jpg", "../images/pic3.jpg", "../images/pic1.jpg"];
-
-let currentIndex = 0;
-
-image.addEventListener("click", () => {
-  image.src = pictures[currentIndex];
-  currentIndex = (currentIndex + 1) % pictures.length;
-});
